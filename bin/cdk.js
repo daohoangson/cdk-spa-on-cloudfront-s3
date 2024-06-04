4bf90aa6da2af1411e62816a49c61cd496522c72
Dao Hoang Son <daohoangson@gmail.com>
2024-06-04 09:21:57 +0700

    Define `WebHostingStack`

--- bin/cdk.js
+++ bin/cdk.js
@@ -3,9 +3,12 @@
 // @ts-check
 
 import * as cdk from "aws-cdk-lib";
-import { ErrorResponsesStack } from "../lib/index.js";
+import { ErrorResponsesStack, WebHostingStack } from "../lib/index.js";
 
 const app = new cdk.App();
 new ErrorResponsesStack(app, "SpaErrorResponses", {
+  description: "https://github.com/daohoangson/cdk-spa-on-cloudfront-s3",
+});
+new WebHostingStack(app, "SpaWebHosting", {
   description: "https://github.com/daohoangson/cdk-spa-on-cloudfront-s3",
 });
